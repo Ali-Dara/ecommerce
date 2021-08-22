837b1e37ea9866cbb042eaeac152e8104f60ba9b
alidara <alireza.irani.ad@gmail.com>
2021-08-22 16:29:18 +0430

    Adding products and images

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./homePage.scss";
+import Directory from "../../components/directiory/directiory.component";
+
+const HomePage = () =>(
+    <div className="homepage">
+        <Directory/>
+    </div>
+)
+export default HomePage;
