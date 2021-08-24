13783e3fc14b2e851403d2ef4e96fade0fddbb11
alidara <alireza.irani.ad@gmail.com>
2021-08-24 16:03:01 +0430

    Adding header to project and set style for this

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import {Route, Switch} from 'react-router-dom';
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
 import Shop from "./components/shop/shop.component";
+import Header from "./components/header/header.component";
 
 function App() {
     return (
         <div className="App">
+            <Header/>
             <Switch>
                 <Route exact path="/" component={HomePage}/>
                 <Route path="/Shop" component={Shop}/>
