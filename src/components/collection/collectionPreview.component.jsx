280704fc15aca2578246fca93dc7aaa32e22e5f1
alidara <alireza.irani.ad@gmail.com>
2021-08-24 15:11:30 +0430

    Adding images for items in collection and set style for this

--- src/components/collection/collectionPreview.component.jsx
+++ src/components/collection/collectionPreview.component.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './collectionPreview.style.scss'
+import CollectionItem from "../collection-item/collection-item.component";
 
 const CollectionPreview = ({title, items}) => (
     <div className="collection-preview">
         <h1 className="title">{title.toUpperCase()}</h1>
         <div className="preview">
-            {items.filter((items, idx) => idx < 4).map(item =>
-                <div>{item.name}</div>
+            {items.filter((items, idx) => idx < 4).map(({id,...otherItemsProps}) =>
+                <CollectionItem key={id} {...otherItemsProps}/>
             )}
         </div>
     </div>
